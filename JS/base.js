b64228f50fc2931e6b3dac758ec89b51c31d312f
Leonardo Salinas Paullo <virtex123x@gmail.com>
2022-02-10 23:52:47 -0500

    hice muy poco

--- JS/base.js
+++ JS/base.js
@@ -1,1 +1,31 @@
-// primera foto en git
+let input_name = document.getElementsByClassName('field-name');
+let input_email = document.getElementsByClassName('field-email');
+let textarea = document.getElementsByClassName('field-msg');
+
+let lbl_name = document.getElementById('name');
+let lbl_email = document.getElementById('email');
+let lbl_msg = document.getElementById('msg');
+
+input_name[0].addEventListener('keyup',function(){
+    if (this.value.length>=1) {
+        lbl_name.classList.add('aux');
+    } else {
+        lbl_name.classList.remove('aux');
+    }
+})
+
+input_email[0].addEventListener('keyup',function(){
+    if (this.value.length>=1) {
+        lbl_email.classList.add('aux');
+    } else {
+        lbl_email.classList.remove('aux');
+    }
+})
+
+textarea[0].addEventListener('keyup',function(){
+    if (this.value.length>=1) {
+        lbl_msg.classList.add('aux');
+    } else {
+        lbl_msg.classList.remove('aux');
+    }
+})
